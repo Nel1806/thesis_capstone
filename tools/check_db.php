695755df244b90f1658723f9fcdb7d4b243b5a20
eiceejohn00 <eiceejohn00@users.noreply.github.com>
2026-05-19 00:33:46 +0800

    Remove generated artifacts from repository

--- tools/check_db.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$db = new PDO('sqlite:database/database.sqlite');
-
-foreach (['audit_imports', 'school_grade_audits', 'audit_sheets', 'users'] as $table) {
-    echo $table.': '.$db->query('select count(*) from '.$table)->fetchColumn().PHP_EOL;
-}
